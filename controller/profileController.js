05de87fa7a6cd79683d3f3f5fc477ec4e91da2c2
Paul Yusuf <paulyusuf2000@gmail.com>
2025-10-16 14:54:11 +0100

    Mock Ready for Deployment

--- controller/profileController.js
+++ controller/profileController.js
@@ -1,33 +1,27 @@
-const axios = require("axios");
-const { timeStamp } = require("console");
-const dotenv = require("dotenv");
-dotenv.config();
+const axios = require('axios');
 
 exports.getProfile = async (req, res) => {
-  try {
-    //  console.log("API", process.env.CAT_API);
-    const response = await axios.get(process.env.CAT_API, {
-      timeout: 5000,
-    });
-    const catFact = response.data.fact;
+    try {
+        const response = await axios.get(process.env.CAT_API, { timeout: 5000 });
+        const catFact = response.data.fact;
 
-    const userData = {
-      status: "Success",
-      user: {
-        email: process.env.EMAIL,
-        name: process.env.NAME,
-        stack: process.env.STACK,
-      },
-      timeStamp: new Date().toISOString(),
-      fact: catFact,
-    };
-    res.status(200).json(userData);
-  } catch (error) {
-    console.error("Cat Fact API Error:", error.message);
-    res.status(500).json({
-      status: "failed",
-      message: "Failed to fetch cat fact.",
-      timeStamp: new Date().toISOString(),
-    });
-  }
+        const userData = {
+            status: "success",
+            user: {
+                email: process.env.EMAIL,
+                name: process.env.NAME,
+                stack: process.env.STACK
+            },
+            timestamp: new Date().toISOString(),
+            fact: catFact
+        };
+        res.status(200).json(userData);
+    } catch (error) {
+        console.error("Cat Fact API Error:", error.message);
+        res.status(500).json({
+            status: "failed",
+            message: "Failed to fetch cat fact.",
+            timestamp: new Date().toISOString()
+        });
+    }
 };
