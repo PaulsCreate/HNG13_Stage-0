412a72b7e1cfbe373c19cc732bcb5465957ebef2
Paul Yusuf <paulyusuf2000@gmail.com>
2025-10-16 12:27:52 +0100

    Cleaned repo

--- routes/profileRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const profileController = require("../controller/profileController");
-
-const route = express.Router();
-
-route.get("/me", profileController.getProfile);
-
-module.exports = route;
